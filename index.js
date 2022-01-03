a3e2c0898a5839704435d74dae850b796e4ed51e
Denisoed <denisoed@gmail.com>
2022-01-03 20:34:31 +0600

    Added env for port

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
+const PORT = process.env.PORT || 7777;
+
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/', routes);
 
-app.listen(7777, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 7777');
 });
