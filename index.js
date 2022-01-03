871f032dd3326dce6d3e873942bfd329444ddfda
Denisoed <denisoed@gmail.com>
2022-01-04 00:14:28 +0600

    Added replace for ":"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 7777;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,5 +12,5 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 app.listen(PORT, () => {
-  console.log('Server is running on port 7777');
+  console.log(`Server is running on port ${PORT}`);
 });
--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const { Grammarly } = require('@stewartmcgown/grammarly-api');
 const router = express.Router();
 
 router.get('/check', async (req, res) => {
-  const text = req.query.text;
+  const text = req.query.text.replace(/:/g, ' ');
   const grammarly = new Grammarly();
   const results = await grammarly.analyse(text);
   res.send(results);
