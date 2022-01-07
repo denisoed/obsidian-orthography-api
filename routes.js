25d1148b51ae3972fdc5ab20aff8df9055d304e6
Denisoed <denisoed@gmail.com>
2022-01-07 23:04:14 +0600

    Changed regex for query text

--- routes.js
+++ routes.js
@@ -4,9 +4,9 @@ const { Grammarly } = require('@stewartmcgown/grammarly-api');
 const router = express.Router();
 
 router.get('/check', async (req, res) => {
-  const text = req.query.text.replace(/:/g, ' ');
+  const formattedText = req.query.text.replace(/;|\/|\?|:|@|&|=|\+|$/g, '%');
   const grammarly = new Grammarly();
-  const results = await grammarly.analyse(text);
+  const results = await grammarly.analyse(formattedText);
   res.send(results);
 });
 
