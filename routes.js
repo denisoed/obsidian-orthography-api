ad40c77e33fd803cb76f42e294ecec40fe671ba4
Denisoed <denisoed@gmail.com>
2022-01-31 21:32:32 +0600

    Minor fixes

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const { Grammarly } = require('@stewartmcgown/grammarly-api');
 const router = express.Router();
 
 router.get('/check', async (req, res) => {
-  const formattedText = req.query.text.replace(/;|\/|\?|:|@|&|=|\+|$/g, '%');
+  const formattedText = req.query.text ? req.query.text.replace(/;|\/|\?|:|@|&|=|\+|$/g, '%') : '';
   const grammarly = new Grammarly();
   const results = await grammarly.analyse(formattedText);
   res.send(results);
